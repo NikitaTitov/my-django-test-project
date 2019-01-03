2ee63d95029080ef74486ca4007991256b820776
Nikita <Nikita.Titov@waveaccess.ru>
2019-01-03 18:28:23 +0300

    Use generic service against manual. Change start url. Add ability to vote in question details

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path, include
+from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.IndexView.as_view(), name='index'),
+    # q_id template name must mapping to template in service level
+    path('<int:pk>', views.DetailView.as_view(), name='details'),
+    path('<int:q_id>/vote', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,34 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
+
+from .models import Question, Choice
 
 
-# Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def index(request):
-    return HttpResponse("Hello, I'm working")
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/details.html'
+
+
+def vote(req, q_id):
+    question = get_object_or_404(Question, pk=q_id)
+    try:
+        selected_choice = question.choices.get(pk=req.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(req, 'polls/details.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:details', args=(question.id,)))
